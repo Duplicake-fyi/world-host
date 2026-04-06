629a4c15867ea2457253def6d4accaf93a47fadd
Duplicake-fyi <notsharingmyemail@no.com>
2026-04-06 23:27:35 +0100

    the sequel to a, introducing the all new, amazing, b!!!!!!!!!!!!!!!!!!!

--- src/main/java/io/github/gaming32/worldhost/mixin/MixinJoinMultiplayerScreen.java
+++ src/main/java/io/github/gaming32/worldhost/mixin/MixinJoinMultiplayerScreen.java
@@ -5,23 +5,37 @@ import io.github.gaming32.worldhost.WorldHostComponents;
 import io.github.gaming32.worldhost.gui.widget.FriendsButton;
 import io.github.gaming32.worldhost.gui.screen.OnlineFriendsScreen;
 import io.github.gaming32.worldhost.versions.ButtonBuilder;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(JoinMultiplayerScreen.class)
-public class MixinJoinMultiplayerScreen extends Screen {
+public abstract class MixinJoinMultiplayerScreen extends Screen {
     @Shadow
     @Final
     private Screen lastScreen;
+
+    @Shadow
+    @Final
+    private HeaderAndFooterLayout layout;
+
+    @Shadow
+    protected abstract void repositionElements();
+
+    @Unique
+    private Button worldHost$serversTab;
+
+    @Unique
+    private FriendsButton worldHost$friendsTab;
 
     protected MixinJoinMultiplayerScreen(Component component) {
         super(component);
@@ -31,29 +45,39 @@ public class MixinJoinMultiplayerScreen extends Screen {
     private void tabs(CallbackInfo ci) {
         if (!WorldHost.CONFIG.isEnableFriends()) return;
 
-        addRenderableWidget(
+        worldHost$serversTab = addRenderableWidget(
             new ButtonBuilder(WorldHostComponents.SERVERS, button -> {})
-                .pos(width / 2 - 102, 32)
+                .pos(0, 0)
                 .width(100)
                 .build()
-        ).active = false;
+        );
+        worldHost$serversTab.active = false;
 
-        addRenderableWidget(new FriendsButton(
-            width / 2 + 2, 32, 100, 20,
+        worldHost$friendsTab = addRenderableWidget(new FriendsButton(
+            0, 0, 100, 20,
             button -> {
                 assert minecraft != null;
                 minecraft.setScreen(new OnlineFriendsScreen(lastScreen));
             }
         ));
+
+        layout.setHeaderHeight(60);
+        worldHost$positionTabs();
+        repositionElements();
     }
 
-    @ModifyConstant(method = "init()V", constant = @Constant(intValue = 32))
-    private int makeTopBigger(int constant) {
-        return WorldHost.CONFIG.isEnableFriends() ? 60 : constant;
+    @Inject(method = "repositionElements()V", at = @At("TAIL"))
+    private void repositionTabs(CallbackInfo ci) {
+        worldHost$positionTabs();
     }
 
-    @ModifyConstant(method = "render", constant = @Constant(intValue = 20))
-    private int moveTitleUp(int constant) {
-        return WorldHost.CONFIG.isEnableFriends() ? 15 : constant;
+    @Unique
+    private void worldHost$positionTabs() {
+        if (!WorldHost.CONFIG.isEnableFriends() || worldHost$serversTab == null || worldHost$friendsTab == null) {
+            return;
+        }
+
+        worldHost$serversTab.setPosition(width / 2 - 102, 40);
+        worldHost$friendsTab.setPosition(width / 2 + 2, 40);
     }
 }
