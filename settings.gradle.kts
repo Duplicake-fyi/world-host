698ecd572ca18c21a2a23d0bdc6166aed84efaf9
Duplicake-fyi <notsharingmyemail@no.com>
2026-04-06 19:03:43 +0100

    fix and clean up github action

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         maven("https://maven.deftu.dev/releases")
         maven("https://maven.fabricmc.net")
         maven("https://maven.architectury.dev/")
-        maven("https://maven.minecraftforge.net")
         maven("https://maven.neoforged.net/releases")
         maven("https://repo.essential.gg/repository/maven-public")
         maven("https://maven.jab125.dev/")
